dff57012cb14de87ff61fa6ebdf0aad617351f1d
IkkiKariu <alexander1stcm@gmail.com>
2025-06-12 18:09:46 +0700

    создание пользователя с правами админимтратора

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-admin {name} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::create([
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => $this->argument('password')
+        ]);
+    }
+}
